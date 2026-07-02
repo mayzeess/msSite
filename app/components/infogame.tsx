a96a2c663a9dcd77170ab53bfae6845ff5c92fc2
mayzeess <ananevviktor22@gmail.com>
2026-07-02 17:42:12 +0400

    оптимизация карточки игры. infogame

--- app/components/infogame.tsx
+++ app/components/infogame.tsx
@@ -17,22 +17,21 @@ type PostGame = {
 
 const InfoGame = ({game}: PostGame) => {
     return(
-        <div>
-            <h1>{game.name}</h1>
-            <div className={styles.imageContainerinfo}>
-                <Image src={game.image} alt={game.name} fill sizes="2000px" style={{objectFit: "cover", objectPosition: "center"} }/>
-            </div>
+        <div className={styles.infoPage}>
             <div className={styles.gamecardinfo}>
-                    <div className={styles.info}>
-                        <h2>{game.name}</h2>
-                        <p>Рейтинг: {game.rating}/10</p>
-                        <p>Комментарий: {game.description}</p>
-                        <div className={styles.buttons}>
-                            <Link href="/game" className={styles.buttoninfo}>Назад</Link>
-                            <Link href={`/game/${game.id}/edit`} className={`${styles.buttoninfo} ${styles.editButton}`}>Редактировать</Link>
-                            <DeleteButton gameId={game.id}/>
-                        </div>
+                <div className={styles.imageContainerinfo}>
+                    <Image src={game.image} alt={game.name} fill sizes="2000px" style={{objectFit: "contain", objectPosition: "center"} }/>
+                </div>
+                <div className={styles.info}>
+                    <h2>{game.name}</h2>
+                    <p>Рейтинг: {game.rating}/10</p>
+                    <p>Комментарий: {game.description}</p>
+                    <div className={styles.buttons}>
+                        <Link href="/game" className={styles.buttoninfo}>Назад</Link>
+                        <Link href={`/game/${game.id}/edit`} className={styles.buttoninfo}>Редактировать</Link>
+                        <DeleteButton gameId={game.id}/>
                     </div>
+                </div>
             </div>
         </div>
     )
